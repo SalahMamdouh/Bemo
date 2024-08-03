1106174abfffcc0024d6d5d510b8aaebe5eef94d
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-03 23:28:27 +0300

    custom card added

---
+++ lib/components/custom_card.dart
@@ -0,0 +1,63 @@
+import 'package:bemo/components/custom_txt.dart';
+import 'package:flutter/material.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({
+    super.key,
+    required this.onTap,
+    required this.cardColor,
+    required this.image,
+    required this.txt,
+  });
+
+  final void Function() onTap;
+  final Color cardColor;
+  final String image;
+  final String txt;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        surfaceTintColor: cardColor,
+        child: Column(
+          children: [
+            // image
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Image.asset(
+                  image,
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            // target location
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 7),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: CustomTxt(
+                txt: txt,
+                color: Colors.black,
+                size: 18,
+                font: true,
+                logo: false,
+                bold: false,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
