f9880e5dadad438ddd7df3e3a2d15fffe6387f17
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-04 00:03:26 +0300

    home page design structure added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'package:bemo/components/custom_card.dart';
+import 'package:bemo/components/custom_txt.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +7,86 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: CustomTxt(
+          txt: 'Bemo',
+          color: Colors.blue.withOpacity(0.7),
+          size: 42,
+          font: true,
+          logo: true,
+          bold: true,
+        ),
+        centerTitle: false,
+        actions: [
+          // add list
+          Container(
+            padding: const EdgeInsets.all(7),
+            margin: const EdgeInsets.only(right: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.grey.withOpacity(0.5),
+                  Colors.grey.withOpacity(0.2),
+                  Colors.grey.withOpacity(0.5),
+                ],
+              ),
+            ),
+            child: Image.asset(
+              'assets/icons/add.png',
+              width: 24,
+              height: 24,
+            ),
+          ),
+
+          // account
+          Container(
+            padding: const EdgeInsets.all(7),
+            margin: const EdgeInsets.only(right: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.grey.withOpacity(0.5),
+                  Colors.grey.withOpacity(0.2),
+                  Colors.grey.withOpacity(0.5),
+                ],
+              ),
+            ),
+            child: Image.asset(
+              'assets/icons/account.png',
+              width: 24,
+              height: 24,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+        ),
+        child: GridView.builder(
+          itemCount: 5,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            return CustomCard(
+              onTap: (){},
+              cardColor: Colors.brown,
+              image: 'assets/icons/work.png',
+              txt: 'Work',
+            );
+          },
+        ),
+      ),
+    );
   }
 }
