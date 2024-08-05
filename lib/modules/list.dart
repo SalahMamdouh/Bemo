4c9b0f0cb316a0046a70e3f7a546a50fd5fd0255
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-05 09:51:24 +0300

    list handling added

---
+++ lib/modules/list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class List {
+  Color color;
+  int icon;
+  String name;
+
+  List({
+    required this.color,
+    required this.icon,
+    required this.name,
+  });
+}
