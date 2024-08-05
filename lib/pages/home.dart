7c78bee548bee5110b7218f1274524fc221433cf
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-05 09:53:05 +0300

    refactoring & list handling

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,92 +1,89 @@
 import 'package:bemo/components/custom_card.dart';
+import 'package:bemo/components/custom_icon_btn.dart';
 import 'package:bemo/components/custom_txt.dart';
+import 'package:bemo/modules/home_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final List lists = [
+    [Colors.blue, 0, 'Custom'],
+    [Colors.brown, 1, 'Work'],
+    [Colors.green, 2, 'School'],
+    [Colors.red, 3, 'Trip'],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: CustomTxt(
-          txt: 'Bemo',
-          color: Colors.blue.withOpacity(0.7),
-          size: 42,
-          font: true,
-          logo: true,
-          bold: true,
-        ),
-        centerTitle: false,
-        actions: [
-          // add list
-          Container(
-            padding: const EdgeInsets.all(7),
-            margin: const EdgeInsets.only(right: 15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.grey.withOpacity(0.5),
-                  Colors.grey.withOpacity(0.2),
-                  Colors.grey.withOpacity(0.5),
-                ],
+    return ChangeNotifierProvider(
+      create: (context) => HomeController(),
+      builder: (context, child) {
+        return Scaffold(
+          appBar: AppBar(
+            title: CustomTxt(
+              txt: 'Bemo',
+              color: Colors.blue.withOpacity(0.7),
+              size: 42,
+              font: true,
+              logo: true,
+              bold: true,
+            ),
+            centerTitle: false,
+            actions: [
+              // add list
+              CustomIconBtn(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/add_list',
+                  );
+                },
+                icon: 'assets/icons/add.png',
               ),
-            ),
-            child: Image.asset(
-              'assets/icons/add.png',
-              width: 24,
-              height: 24,
-            ),
-          ),
 
-          // account
-          Container(
-            padding: const EdgeInsets.all(7),
-            margin: const EdgeInsets.only(right: 15),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Colors.grey.withOpacity(0.5),
-                  Colors.grey.withOpacity(0.2),
-                  Colors.grey.withOpacity(0.5),
-                ],
+              // account
+              CustomIconBtn(
+                onTap: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/account',
+                  );
+                },
+                icon: 'assets/icons/account.png',
               ),
+            ],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 15,
             ),
-            child: Image.asset(
-              'assets/icons/account.png',
-              width: 24,
-              height: 24,
+            child: GridView.builder(
+              itemCount: lists.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 15,
+                crossAxisSpacing: 15,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return CustomCard(
+                  onTap: () {},
+                  cardColor: lists[index][0],
+                  image: lists[index][1] == 0
+                      ? 'assets/icons/daily_use.png'
+                      : lists[index][1] == 1
+                          ? 'assets/icons/work.png'
+                          : lists[index][1] == 2
+                              ? 'assets/icons/school.png'
+                              : 'assets/icons/trip.png',
+                  txt: lists[index][2],
+                );
+              },
             ),
           ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 15,
-        ),
-        child: GridView.builder(
-          itemCount: 5,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            mainAxisSpacing: 15,
-            crossAxisSpacing: 15,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            return CustomCard(
-              onTap: (){},
-              cardColor: Colors.brown,
-              image: 'assets/icons/work.png',
-              txt: 'Work',
-            );
-          },
-        ),
-      ),
+        );
+      },
     );
   }
 }
