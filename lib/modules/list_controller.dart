cd8f9b1348ce9e156a5b5ccc348a83ca729a07ca
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-06 10:15:30 +0300

    modified

--- lib/modules/list_controller.dart
+++ lib/modules/list_controller.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 class ListController with ChangeNotifier {
+  int _type = -1;
 
+  int get type => _type;
+
+  void setType(int type) {
+    _type = type;
+    notifyListeners();
+  }
 }
