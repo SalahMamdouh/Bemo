cbbc29efb70c37f58bd07c215bcd39cff305bb33
Salah Mamdouh <salahmamdouh.ali@gmail.com>
2024-08-06 22:27:39 +0300

    state management modifications

--- lib/modules/list_controller.dart
+++ lib/modules/list_controller.dart
@@ -2,9 +2,24 @@ import 'package:flutter/cupertino.dart';
 
 class ListController with ChangeNotifier {
   int _type = -1;
+  int _page = 0;
 
   int get type => _type;
+  int get page => _page;
 
+  // next indicators
+  void nextIndicator() {
+    _page == 2? null : _page++;
+    notifyListeners();
+  }
+
+  // previous indicators
+  void previousIndicator() {
+    _page == 0? null : _page--;
+    notifyListeners();
+  }
+
+  // set list types
   void setType(int type) {
     _type = type;
     notifyListeners();
